0e5d7d840ebab53c2c0643d56574c7361a16c1fd
Sid00721 <60917616+Sid00721@users.noreply.github.com>
2025-07-23 18:17:25 +1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -54,19 +54,3 @@ const App = () => {
 };
 
 export default App;
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
